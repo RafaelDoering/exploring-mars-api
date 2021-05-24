644da50828e94556e5adbe2241975cbc6262c901
RafaelDoering <rafael.doering.soares@usp.br>
2021-05-24 00:53:23 -0300

    Initial commit

---
+++ src/main/java/com/rafaeldoering/mars/ExploringMarsApiApplication.java
@@ -0,0 +1,13 @@
+package com.rafaeldoering.mars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploringMarsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploringMarsApiApplication.class, args);
+	}
+
+}
